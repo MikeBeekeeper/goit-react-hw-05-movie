c03a5c89fea32538b9093a1ac9fdfbbeb1119e66
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-10-06 11:20:18 +0300

    final fix

--- src/helpers/GetImage.js
+++ src/helpers/GetImage.js
@@ -1,3 +1,3 @@
 export const getImage = image => {
-    if (image) return `https://image.tmdb.org/t/p/w500${image}`;
+    if (image) return `https://image.tmdb.org/t/p/w500${image}`
   };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,10 @@ import TrendingMoviesList from "components/trendingMoviesList/TrendingMovieList"
 const HomePage = () => {
 
     return (
+        <>
+        <h1>Trending movies today</h1>
         <TrendingMoviesList/>
+        </>
     )
 }
 
